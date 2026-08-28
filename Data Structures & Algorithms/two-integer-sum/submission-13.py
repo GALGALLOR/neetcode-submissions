98fe37e720e83671d9da5788682d8313b978c0da
Galgallo Roba Waqo <69774313+GALGALLOR@users.noreply.github.com>
2026-08-28 14:01:32 -0500

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mymap = {}
+        for i,num in enumerate(nums):
+            if target-num in mymap:
+                return [mymap[target-num],i]
+            else:
+                if num not in mymap:
+                    mymap[num] = i
+        return []
+            
