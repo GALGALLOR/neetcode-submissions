6a9cf0e0f4bdce8f3ec7723b8392939cc1bf7a15
Galgallo Roba Waqo <69774313+GALGALLOR@users.noreply.github.com>
2026-09-03 09:06:23 -0500

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #find remaining Time for each car
+        p_t = [(position[i],(target-position[i])/speed[i]) for i in range(len(position))]
+        p_t.sort(reverse=True)
+        stack = []
+        for p,t in p_t:
+            if not stack or t>stack[-1]:
+                stack.append(t)
+        
+        
+        return len(stack)
+        
