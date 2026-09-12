dbee11472b5904823a3bec73d62fed08e0b1b9ad
Galgallo Roba Waqo <69774313+GALGALLOR@users.noreply.github.com>
2026-09-12 16:21:47 -0500

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #make it lowercase
+        #remove spaces
+        p1 = 0
+        p2 = len(s)-1
+        while p1<p2:
+            if not s[p1].isalnum():
+                p1+=1
+                continue
+            if not s[p2].isalnum():
+                p2-=1
+                continue
+            if s[p1].lower() == s[p2].lower():
+                p2-=1
+                p1+=1
+            else:
+                return False
+        return True
+                
+
+        
